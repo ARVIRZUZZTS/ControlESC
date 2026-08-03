71a32e36941e9e609c507e616e9015ae268b3a37
ARVIRZUZZTS <virzuzz12345@gmail.com>
2026-08-02 23:46:26 -0400

    bd

--- bd/control_exp.sql
+++ bd/control_exp.sql
@@ -14,27 +14,26 @@ INSERT INTO `unidad_aceite` (`id_ua`, `unidad_aceite`, `conversion`) VALUES
 --
 CREATE TABLE `marca_aceite` (
   `id_marca_aceite` int(11) NOT NULL primary key auto_increment,
-  `marca_aceite` varchar(100) NOT NULL,
-  `espesor` decimal(10,2) DEFAULT NULL,
+  `nombre_marca_aceite` varchar(100) NOT NULL,
   `precio` decimal(10,2) DEFAULT NULL,
-  `unidad_aceite` int(11) DEFAULT NULL,
+  `id_ua` int(11) DEFAULT NULL,
   `media_viajes` int(11) DEFAULT NULL
 );
-INSERT INTO `marca_aceite` (`id_marca_aceite`, `marca_aceite`, `espesor`, `precio`, `unidad_aceite`, `media_viajes`) VALUES
-(1, 'marca', 5.00, 150.00, 2, 30),
-(2, 'santos', 10.00, 300.00, 1, 30);
-
-CREATE TABLE `aceite_reporte` (
-  `id_ar` int(11) not null primary key auto_increment,
+INSERT INTO `marca_aceite` (`id_marca_aceite`, `nombre_marca_aceite`, `precio`, `id_ua`, `media_viajes`) VALUES
+(1, 'marca', 150.00, 2, 30),
+(2, 'santos', 300.00, 1, 30);
+-- -------------------------------------------------------------------
+CREATE TABLE `aceite_lote` (
+  `id_al` int(11) not null primary key auto_increment,
   `id_marca_aceite` int(11) DEFAULT NULL,
   `precio_total` decimal(10,2) DEFAULT NULL,
   `precio_unitario` decimal(10,2) DEFAULT NULL,
-  `unidad_aceite` int(11) DEFAULT NULL,
+  `id_ua` int(2) DEFAULT NULL,
   `cantidad` decimal(10,3) DEFAULT NULL,
   `stock` decimal(10,3) DEFAULT NULL,
   `fecha_compra` date DEFAULT NULL
 );
-INSERT INTO `aceite_reporte` (`id_marca_aceite`, `cantidad`, `precio_total`, `precio_unitario`, `unidad_aceite`, `fecha_compra`) VALUES
+INSERT INTO `aceite_lote` (`id_marca_aceite`, `cantidad`, `precio_total`, `precio_unitario`, `id_ua`, `fecha_compra`) VALUES
 (1, 2.000, 300.00, 150.00, 3, '2026-04-03'),
 (1, 1.000, 300.00, 300.00, 2, '2026-04-02'),
 (1, 3.000, 300.00, 100.00, 1, '2026-04-01'),
@@ -49,7 +48,7 @@ INSERT INTO `aceite_reporte` (`id_marca_aceite`, `cantidad`, `precio_total`, `pr
 
 CREATE TABLE `aceite_flota` (
   `id_af` int(11) primary key auto_increment,
-  `id_ar` int(11) DEFAULT NULL,
+  `id_al` int(11) DEFAULT NULL,
   `placa` varchar(10) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
   `unidad_aceite` decimal(10,2) DEFAULT NULL,
@@ -66,18 +65,6 @@ CREATE TABLE `empleado` (
   `id_te` decimal(10,2) DEFAULT NULL,
   `fecha_contrato` date DEFAULT NULL
 );
-
-CREATE TABLE `tipo_empleado` (
-  `id_te` int(11) primary key auto_increment,
-  `tipo_empleado` varchar(50) DEFAULT NULL
-);
-
-insert into `tipo_empleado` (`tipo_empleado`) values 
-('Chofer Principal'),
-('Chofer Auxiliar'), 
-('Ayudante'),
-('Boletero'), 
-('Ayudante');
 INSERT INTO `empleado` (`empleado`, `mensual`, `total`, `id_te`, `fecha_contrato`) VALUES
 ('ERICK MENDOZA MONTESINOS', 1500.00, 0.00, 1, '2026-04-10'),
 ('WILSON LAZARTE MONTAÑO', 1500.00, 0.00, 1, '2026-04-10'),
@@ -96,8 +83,20 @@ INSERT INTO `empleado` (`empleado`, `mensual`, `total`, `id_te`, `fecha_contrato
 ('ARIEL VARGAS VALLEJOS', 1500.00, 0.00, 1, '2026-04-10'),
 ('JHONNY VARGAS HERBAS', 1500.00, 0.00, 1, '2026-04-10'),
 ('HUGO GABRIEL RASGUIDO', 1500.00, 0.00, 1, '2026-04-10');
+-- --------------------------------------------------------
 
+CREATE TABLE `tipo_empleado` (
+  `id_te` int(11) primary key auto_increment,
+  `tipo_empleado` varchar(50) DEFAULT NULL
+);
 
+insert into `tipo_empleado` (`tipo_empleado`) values 
+('Chofer Principal'),
+('Chofer Auxiliar'), 
+('Ayudante'),
+('Boletero'), 
+('Ayudante');
+-- --------------------------------------------------------
 create table `empleado_reporte` (
   `id_er` int(11) primary key auto_increment,
   `id_empleado` int(11) DEFAULT NULL,
@@ -107,7 +106,6 @@ create table `empleado_reporte` (
   `id_gasto` int(11) DEFAULT NULL
 );
 -- --------------------------------------------------------
--- FLOTA
 CREATE TABLE `flota` (
   `placa` varchar(10) NOT NULL primary key,
   `propietario` varchar(150) DEFAULT NULL,
@@ -117,7 +115,6 @@ CREATE TABLE `flota` (
   `ubicacion` varchar(20) DEFAULT NULL,
   `viajes` int(11) DEFAULT NULL
 );
--- Data
 INSERT INTO `flota` (`placa`, `propietario`, `chofer1`, `chofer2`, `estado`, `ubicacion`, `viajes`) VALUES
 ('1194-UKE', 'ERICK MENDOZA', 1, 0, 'Parqueado', 'Cochabamba', 0),
 ('1461-KUX', 'VIVIAN CABALLERO', 2, 0, 'Parqueado', 'Cochabamba', 0),
@@ -147,11 +144,10 @@ CREATE TABLE `reporte` (
   `liq_encomiendas` decimal(10,2) DEFAULT NULL,
   `diesel_partida` decimal(10,2) DEFAULT NULL,
   `diesel_llegada` decimal(10,2) DEFAULT NULL,
-  `dpFok` boolean DEFAULT NULL,
-  `drFok` boolean DEFAULT NULL
+  `dpFactura` boolean DEFAULT NULL,
+  `drFactura` boolean DEFAULT NULL
 );
 -- --------------------------------------------------------
--- Estructura de tabla para la tabla `gasto`
 CREATE TABLE `gasto` (
   `id_gasto` int(11) NOT NULL,
   `id_rep` int(11) DEFAULT NULL,
@@ -159,14 +155,13 @@ CREATE TABLE `gasto` (
   `descripcion` varchar(1000) DEFAULT NULL,
   `gasto` decimal(10,2) DEFAULT NULL
 );
--- Estructura de tabla para la tabla `descripcion`
+-- --------------------------------------------------------
 CREATE TABLE `descripcion` (
   `id_descripcion` int(11) NOT NULL primary key auto_increment,
   `descripcion` varchar(500) DEFAULT NULL,
   `precio` decimal(10,2) DEFAULT NULL
 );
 -- --------------------------------------------------------
--- Estructura de tabla para la tabla `detalle`
 CREATE TABLE `detalle` (
   `id_detalle` int(11) NOT NULL primary key auto_increment,
   `id_descripcion` int(11) DEFAULT NULL,
@@ -174,39 +169,38 @@ CREATE TABLE `detalle` (
   `tipo` varchar(10) DEFAULT NULL
 );
 -- -------------------------------------------------------- here
--- Estructura de tabla para la tabla `marca_rueda`
 CREATE TABLE `marca_rueda` (
   `id_marca_rueda` int(11) NOT NULL primary key auto_increment,
-  `marca_rueda` varchar(100) NOT NULL,
-  `diametro` decimal(10,2) DEFAULT NULL,
-  `grosor` decimal(10,2) DEFAULT NULL,
-  `espesor` decimal(10,2) DEFAULT NULL,
-  `precio` decimal(10,2) DEFAULT NULL,
+  `nombre_marca_rueda` varchar(100) NOT NULL,
+  `medida` decimal(10,2) DEFAULT NULL,
+  `serie` varchar(50) DEFAULT NULL,
+  `trilla` varchar(50) DEFAULT NULL,
+  `aro` varchar(50) DEFAULT NULL,
   `media_viajes` int(11) DEFAULT NULL
 );
--- Volcado de datos para la tabla `marca_rueda`
-INSERT INTO `marca_rueda` (`id_marca_rueda`, `marca_rueda`, `diametro`, `grosor`, `espesor`, `media_viajes`) VALUES
-(1, 'MarcaR1', 49.50, 4.50, 2.00, 30),
-(2, 'MarcaR2', 48.50, 4.00, 2.50, 30);
 -- --------------------------------------------------------
--- Estructura de tabla para la tabla `rueda`
-CREATE TABLE `rueda_reporte` (
-  `id_rr` int(11) NOT NULL primary key auto_increment,
+CREATE TABLE `rueda_lote` (
+  `id_rl` int(11) NOT NULL primary key auto_increment,
   `id_marca_rueda` int(11) DEFAULT NULL,
-  `placa` varchar(10) NOT NULL DEFAULT '-',
   `precio_unitario` decimal(10,2) DEFAULT NULL,
   `precio_total` decimal(10,2) DEFAULT NULL,
   `cantidad` decimal(10,3) DEFAULT NULL,
   `stock` decimal(10,3) DEFAULT NULL,
   `fecha_compra` date DEFAULT NULL
 );
-
+-- --------------------------------------------------------
 CREATE TABLE `rueda_flota` (
   `id_rf` int(11) NOT NULL primary key auto_increment,
-  `id_rr` int(11) DEFAULT NULL,
+  `id_rl` int(11) DEFAULT NULL,
   `placa` varchar(10) NOT NULL DEFAULT '-',
   `codigo` varchar(20) NOT NULL,
   `viajes_hechos` int(11) DEFAULT NULL,
   `estado` varchar(20) DEFAULT NULL,
   `fecha_uso` date DEFAULT NULL
+);
+-- --------------------------------------------------------
+CREATE TABLE `posicion_rueda` (
+  `id_pr` int(11) NOT NULL primary key auto_increment,
+  `nombre_posicion` varchar(50) NOT NULL DEFAULT '-',
+  `placa` varchar(10) NOT NULL  
 );
